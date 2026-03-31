3998a8ea9584c4feb5e4821974ca4810999904bc
Bio Paul Kobena <biokobena07@gmail.com>
2026-03-31 16:52:36 +0200

    feat: components create

--- event-manage/src/app/app.routes.ts
+++ event-manage/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ConcertComponent } from './concert/concert.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "/dashboard",
+    component: DashboardComponent
+}, {
+    path: "/concert",
+    component: ConcertComponent
+}];
---
+++ event-manage/src/app/artiste/artiste.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artiste',
+  imports: [],
+  templateUrl: './artiste.component.html',
+  styleUrl: './artiste.component.css'
+})
+export class ArtisteComponent {
+
+}
